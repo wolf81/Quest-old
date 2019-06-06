ed7c7979167cab6c94b7c4b41376b676a57ded49
Wolfgang Schreurs <wolfgang@wolftrail.net>
2019-06-06 17:24:12 +0700

    Add player sprites

---
+++ Quest Shared/Monster.swift
@@ -0,0 +1,13 @@
+//
+//  Monster.swift
+//  Quest
+//
+//  Created by Wolfgang Schreurs on 06/06/2019.
+//  Copyright © 2019 Wolftrail. All rights reserved.
+//
+
+import Foundation
+
+class Monster : Entity {
+    
+}
