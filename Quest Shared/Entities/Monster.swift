aa5480f264eae477f6f8f398fdb8487b86fc07b9
Wolfgang Schreurs <wolfgang@wolftrail.net>
2020-05-23 16:21:15 +0700

    WIP: make monsters with ranged attacks move to hero when out of range

--- Quest Shared/Entities/Monster.swift
+++ Quest Shared/Entities/Monster.swift
@@ -41,65 +41,93 @@ class Monster: Actor, CustomStringConvertible {
     var description: String {
         return "\(self.name) [ HD: \(self.hitDie) / HP: \(self.hitPoints.current) / AC: \(self.armorClass) ]"
     }
-    
+        
     override func update(state: Game) {
         guard self.isAlive else { return }
-                
+                        
+        guard state.hero.isAlive else {
+            return idle()
+        }
+
+        let didMeleeAttack = attack(meleeTarget: state.hero, state: state)
+        guard didMeleeAttack == false else { return }
+
+        let didRangedAttack = attack(rangedTarget: state.hero, state: state)
+        guard didRangedAttack == false else { return }
+        
+        let didMove = move(to: state.hero, state: state)
+        print("didMove: \(didMove)")
+    }
+    
+    // MARK: - Private
+    
+    private func attack(rangedTarget: Actor, state: Game) -> Bool {
+        guard self.energy.amount >= self.energyCost.attack else { return false }
+
+        guard state.actorVisibleCoords.contains(rangedTarget.coord) else { return false }
+
+        let coords = Functions.coordsBetween(self.coord, rangedTarget.coord)
+        let tiles = coords.compactMap({ state.getTile(at: $0) })
+        let isBlocked = tiles.contains(1)
+        let isRangedWeaponEquipped = self.equippedWeapon.range > 1
+        // if the hero is not blocked by walls and we carry a ranged weapon, shoot on the hero
+        
+        guard isBlocked == false && isRangedWeaponEquipped else { return false }
+        
+        let x = pow(Float(rangedTarget.coord.x - self.coord.x), 2)
+        let y = pow(Float(rangedTarget.coord.x - self.coord.x), 2)
+        let distance = Int(sqrt(x + y))
+
+        guard distance <= self.equippedWeapon.range else { return false }
+
+        let attack = RangedAttackAction(actor: self, targetActor: rangedTarget)
+        setAction(attack)
+
+        return true
+    }
+    
+    private func attack(meleeTarget: Actor, state: Game) -> Bool {
+        guard self.energy.amount >= self.energyCost.attack else { return false }
+
         // If hero is in melee range, perform melee attack
         let xRange = self.coord.x - 1 ... self.coord.x + 1
         let yRange = self.coord.y - 1 ... self.coord.y + 1
-        if state.hero.isAlive && xRange.contains(state.hero.coord.x) && yRange.contains(state.hero.coord.y) {
-            guard self.energy.amount >= self.energyCost.attack else { return }
-            let attack = MeleeAttackAction(actor: self, targetActor: state.hero)
-            return setAction(attack)
-        }
-                        
-        if state.actorVisibleCoords.contains(state.hero.coord) && state.hero.isAlive {
-            let coords = Functions.coordsBetween(self.coord, state.hero.coord)
-            let tiles = coords.compactMap({ state.getTile(at: $0) })
-            let isBlocked = tiles.contains(1)
-
-            // if the hero is not blocked by walls and we carry a ranged weapon, shoot on the hero
-            if isBlocked == false {
-                if self.equippedWeapon.range > 1 {
-                    if self.energy.amount >= self.energyCost.attack {
-                        let x = pow(Float(state.hero.coord.x - self.coord.x), 2)
-                        let y = pow(Float(state.hero.coord.x - self.coord.x), 2)
-                        let distance = Int(sqrt(x + y))
-                        
-                        if distance <= self.equippedWeapon.range {
-                            let attack = RangedAttackAction(actor: self, targetActor: state.hero)
-                            return setAction(attack)
-                        }
-                    }
-                }
-            } else {
-                if self.energy.amount >= self.energyCost.move {}
-                guard self.energy.amount >= self.energyCost.move else { return }
-                let actorCoords = state.activeActors.filter({ $0.coord != self.coord && $0.coord != state.hero.coord }).compactMap({ $0.coord })
-                let movementGraph = state.getMovementGraph(for: self, range: self.sight, excludedCoords: actorCoords)
-                if let actorNode = movementGraph.node(atGridPosition: self.coord), let heroNode = movementGraph.node(atGridPosition: state.hero.coord)  {
-                    var pathNodes = movementGraph.findPath(from: actorNode, to: heroNode) as! [GKGridGraphNode]
-                    pathNodes.removeLast() // we don't want to move on top of the hero
-
-                    let moveCount = 1
-                    
-                    if pathNodes.count > 0 && moveCount > 0 {
-                        let nodeCount = pathNodes.count
-                        
-                        let removeNodeCount = max(nodeCount - moveCount - 1, 0)
-                        pathNodes.removeLast(removeNodeCount)
-                                            
-                        let path = pathNodes.compactMap({ $0.gridPosition })
-                        let move = MoveAction(actor: self, coords: path)
-                        return setAction(move)
-                    }
-                }
-            }
-        }
+        let targetInMeleeRange = xRange.contains(meleeTarget.coord.x) && yRange.contains(meleeTarget.coord.y)
         
-        let idle = IdleAction(actor: self)
-        setAction(idle)
+        guard targetInMeleeRange else { return false }
+
+        let attack = MeleeAttackAction(actor: self, targetActor: meleeTarget)
+        setAction(attack)
+        return true
+    }
+    
+    private func idle() {
+        setAction(IdleAction(actor: self))
+    }
+    
+    private func move(to actor: Actor, state: Game) -> Bool {
+        guard self.energy.amount >= self.energyCost.move else { return false }
+
+        guard state.actorVisibleCoords.contains(actor.coord) else { return false }
+
+        let actorCoords = state.activeActors.filter({ $0.coord != self.coord && $0.coord != actor.coord }).compactMap({ $0.coord })
+        let movementGraph = state.getMovementGraph(for: self, range: self.sight, excludedCoords: actorCoords)
+        
+        guard let actorNode = movementGraph.node(atGridPosition: self.coord), let targetNode = movementGraph.node(atGridPosition: actor.coord) else { return false }
+        
+        var pathNodes = movementGraph.findPath(from: actorNode, to: targetNode) as! [GKGridGraphNode]
+        pathNodes.removeLast() // we don't want to move on top of the hero
+
+        guard pathNodes.count > 0 else { return false }
+        
+        let removeNodeCount = max(pathNodes.count - 2, 0)
+        pathNodes.removeLast(removeNodeCount)
+                            
+        let path = pathNodes.compactMap({ $0.gridPosition })
+        let move = MoveAction(actor: self, coords: path)
+        setAction(move)
+        
+        return true
     }
 }
 
