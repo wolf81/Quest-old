507a8220c4e29de8edf082b0a1bb3fd886f06192
Wolfgang Schreurs <wolfgang@wolftrail.net>
2020-05-27 01:09:56 +0700

    Code clean-up

--- Quest Shared/Functions.swift
+++ Quest Shared/Functions.swift
@@ -8,9 +8,8 @@
 
 import simd
 
-// Based on: https://github.com/RockLobster/Bresenham-Swift/blob/master/Bresenham/Bresenham.swift
-
 struct Functions {
+    // Based on: https://github.com/RockLobster/Bresenham-Swift/blob/master/Bresenham/Bresenham.swift
     static func coordsBetween(_ start: vector_int2, _ end: vector_int2) -> [vector_int2] {
         var p1 = start
         var p2 = end
@@ -27,12 +26,20 @@ struct Functions {
         
         return internalCoordsBetween(p1, p2, isSteep: isSteep)
     }
-    
+        
+    /// From a position and radius return a range that is constrained to another range, e.g. if we wanted to get a range of numbers with
+    /// origin 5 and radius 3 and a constrained to the range [4 ... 10], this function would return the following range [4, 5, 6, 7, 8]
+    /// - Parameters:
+    ///   - origin: The start position for the range calculation
+    ///   - radius: The radius for which we want to return a new range
+    ///   - range: A range that is used to constrain the values returned to some minimum and maximum value
     static func getRange<T: BinaryInteger>(origin: T, radius: T, constrainedTo range: Range<T>) -> Range<T> {
         let minValue = max(origin - radius, range.lowerBound)
         let maxValue = min(origin + radius + 1, range.upperBound)
         return T(minValue) ..< T(maxValue)
     }
+    
+    // MARK: - Private
     
     private static func internalCoordsBetween(_ start: vector_int2, _ end: vector_int2, isSteep: Bool) -> [vector_int2] {
         let dx = end.x - start.x
